9aa369d4cbe97ff2494a91e0d8e5a5621cf9c035
iastanley <iasvks.craigslist@gmail.com>
2017-03-29 21:23:15 -0700

    made first version models.js

---
+++ models.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+//set up schema for a blog post
+const postSchema = mongoose.Schema({
+  title: String
+  content: String
+  author: {
+    firstName: String,
+    lastName: String
+  }
+});
+
+//create a virtual property for fullName
+postSchema.virtual('fullName').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`;
+});
+
+//instance method for return format for API
+postSchema.methods.apiReturn = function() {
+  return {
+    id: this._id,
+    title: this.title,
+    author: this.fullName,
+    content: this.content
+  }
+}
+
+//create the model for the database
+const Post = mongoose.model('Post', postSchema);
+
+//export the model
+module.exports = {Post};
